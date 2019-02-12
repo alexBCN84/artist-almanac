9ad396baa29f51353e71cbbb4a29edbf55afcd33
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2019-02-12 07:57:13 +0100

    UI styling removeButton and no eventsBox

--- src/components/ArtistGallery.js
+++ src/components/ArtistGallery.js
@@ -9,19 +9,46 @@ import EventsGallery from './EventsGallery';
 import { Grid, Row, Col } from './UI/Grid';
 
 const artistGallery = props => {
+
+    const noEventsBoxStyle = {
+        width: "100%",
+        padding: 7,
+        backgroundColor: "#1B1C1C",
+        lineHeight: "1.4em",
+        fontSize: ".8em",
+        color: "#fff",
+        textAlign: "center",
+        borderRadius: 4,
+        border: "1px solid #000"
+    }
+
+    const removeButtonStyles = {
+        backgroundColor: "#57B3B1",
+        color: "#fff",
+        borderRadius: 4,
+        border: "1px solid #57B3B1",
+        padding: 6
+    }
     return (
         <Grid>
             <Row>
 
                 {props.storedArtists.map(artist =>
-                    <Col mobile={12} phablet={5} desktop={3} key={artist.id}>
+                    <Col mobile={12} phablet={6} tablet={5} desktop={3} key={artist.id}>
                         <ArtistCard >
                             <ProfileCard {...selectProps(artist, ['profile_facebook', 'profile_id', 'profile_image', 'profile_name', 'profile_upcoming_event_count'])} />
                             <EventsGallery>
-                                {artist.events.map(event => <EventCard key={event.id} {...event} />)}
+                                <Grid gutters={10}>
+                                    <Row>
+                                        {artist.events.length === 0 ?
+                                            <Col mobile={8}><p style={noEventsBoxStyle}>There are no upcoming events for {artist.profile_name}</p></Col> :
+                                            artist.events.map(event => <Col mobile={5} key={event.id}><EventCard {...event} /></Col>)}
+                                    </Row>
+                                </Grid>
+
                             </EventsGallery>
                         </ArtistCard>
-                        <button onClick={() => props.onRemoveArtist(artist.id)}>Remove artist from gallery</button>
+                        <button style={removeButtonStyles} onClick={() => props.onRemoveArtist(artist.id)}>Remove artist from gallery</button>
                     </Col>)
                 }
             </Row>
