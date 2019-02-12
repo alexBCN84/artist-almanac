f10e45d8d1b8d8e822f1d9f1c018d6b35b52ecca
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2019-02-12 07:57:43 +0100

    UI rendering events data into eventCard

--- src/components/EventsGallery.js
+++ src/components/EventsGallery.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Aux from '../hoc/Aux';
-
-const eventsGallery = props => <Aux>{props.children}</Aux>
+const eventsGallery = props => {
+    return (
+        <Aux>{props.children}</Aux>
+    )
+}
 
 export default eventsGallery;
