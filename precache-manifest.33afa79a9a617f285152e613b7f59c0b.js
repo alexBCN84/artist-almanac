3022faa03bb77a880abed24ff3c0c6ee19640306
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2019-02-14 16:49:23 +0100

    Updates

---
+++ precache-manifest.33afa79a9a617f285152e613b7f59c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c64029e8834303180c50",
+    "url": "/artist-almanac/static/js/runtime~main.c64029e8.js"
+  },
+  {
+    "revision": "da6b5a59a6f10fc85d34",
+    "url": "/artist-almanac/static/js/main.da6b5a59.chunk.js"
+  },
+  {
+    "revision": "c7a445f6a85768c534bd",
+    "url": "/artist-almanac/static/js/1.c7a445f6.chunk.js"
+  },
+  {
+    "revision": "da6b5a59a6f10fc85d34",
+    "url": "/artist-almanac/static/css/main.cb036968.chunk.css"
+  },
+  {
+    "revision": "36c997a4cc869f9184528615b7c0c72e",
+    "url": "/artist-almanac/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/artist-almanac/precache-manifest.33afa79a9a617f285152e613b7f59c0b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/artist-almanac/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
